876f2b02075d3bc989bd189b4b165eb59f60a259
pts5di <56211218+pts5di@users.noreply.github.com>
2021-09-07 22:27:46 -0700

    Map functionality to all vJoy buttons

--- HandPreservationTool/MainWindow.xaml.cs
+++ HandPreservationTool/MainWindow.xaml.cs
@@ -61,6 +61,8 @@ namespace HandPreservationTool
                 VjdStat status = joystick.GetVJDStatus(chosenVJoyId);
                 joystick.AcquireVJD(chosenVJoyId);
 
+                var initialBias = new vJoy.JoystickState();
+
                 while (true)
                 {
                     var str = "";
@@ -83,6 +85,10 @@ namespace HandPreservationTool
                             // just pick the first one forever (for now -- we will
                             // eventually want to add a dropdown to let the user pick)
                             chosenXboxId = i;
+                            initialBias.AxisX = newState.Gamepad.sThumbLX;
+                            initialBias.AxisY = newState.Gamepad.sThumbLY;
+                            initialBias.AxisXRot = newState.Gamepad.sThumbRX;
+                            initialBias.AxisYRot = newState.Gamepad.sThumbRY;
                         }
                         foreach (XInput.XInputGamepadButton button in Enum.GetValues(typeof(XInput.XInputGamepadButton)))
                         {
@@ -92,6 +98,9 @@ namespace HandPreservationTool
                                 str += $"Controller {i}: {buttonName} is pressed\n";
                             }
                         }
+
+                        str += $"Controller {i}: Left Stick: {newState.Gamepad.sThumbLX}X, {newState.Gamepad.sThumbLY}Y\n";
+                        str += $"Controller {i}: Right Stick: {newState.Gamepad.sThumbRX}X, {newState.Gamepad.sThumbRY}Y\n";
                     }
 
                     var chosenXboxState = new XInput.XInputState();
@@ -99,20 +108,34 @@ namespace HandPreservationTool
                     var xPad = chosenXboxState.Gamepad;
                     var xPadButtons = (uint)xPad.wButtons;
 
-                    uint aButton = (uint)((xPadButtons & (0x1 << 12)) > 0 ? 0x1 << 0 : 0);
-                    uint bButton = (uint)((xPadButtons & (0x1 << 13)) > 0 ? 0x1 << 1 : 0);
+                    uint aButton = (uint)((xPadButtons & ((uint)XInput.XInputGamepadButton.A)) > 0 ? 0x1 << 0 : 0);
+                    uint bButton = (uint)((xPadButtons & ((uint)XInput.XInputGamepadButton.B)) > 0 ? 0x1 << 1 : 0);
+                    uint xButton = (uint)((xPadButtons & ((uint)XInput.XInputGamepadButton.X)) > 0 ? 0x1 << 2 : 0);
+                    uint yButton = (uint)((xPadButtons & ((uint)XInput.XInputGamepadButton.Y)) > 0 ? 0x1 << 3 : 0);
+                    uint lBumper = (uint)((xPadButtons & ((uint)XInput.XInputGamepadButton.LeftShoulder)) > 0 ? 0x1 << 4 : 0);
+                    uint rBumper = (uint)((xPadButtons & ((uint)XInput.XInputGamepadButton.RightShoulder)) > 0 ? 0x1 << 5 : 0);
+                    uint select = (uint)((xPadButtons & ((uint)XInput.XInputGamepadButton.Back)) > 0 ? 0x1 << 6 : 0);
+                    uint start = (uint)((xPadButtons & ((uint)XInput.XInputGamepadButton.Start)) > 0 ? 0x1 << 7 : 0);
+                    uint lStick = (uint)((xPadButtons & ((uint)XInput.XInputGamepadButton.LeftThumb)) > 0 ? 0x1 << 8 : 0);
+                    uint rStick = (uint)((xPadButtons & ((uint)XInput.XInputGamepadButton.RightThumb)) > 0 ? 0x1 << 9 : 0);
+                    uint guide = (uint)((xPadButtons & ((uint)XInput.XInputGamepadButton.Guide)) > 0 ? 0x1 << 10 : 0);
+                    uint dPadUp = (uint)((xPadButtons & ((uint)XInput.XInputGamepadButton.DPadUp)) > 0 ? 0x1 << 11 : 0);
+                    uint dPadDown = (uint)((xPadButtons & ((uint)XInput.XInputGamepadButton.DPadDown)) > 0 ? 0x1 << 12 : 0);
+                    uint dPadLeft = (uint)((xPadButtons & ((uint)XInput.XInputGamepadButton.DPadLeft)) > 0 ? 0x1 << 13 : 0);
+                    uint dPadRight = (uint)((xPadButtons & ((uint)XInput.XInputGamepadButton.DPadRight)) > 0 ? 0x1 << 14 : 0);
 
-                    uint buttons = aButton | bButton;
+                    uint buttons = aButton | bButton | xButton | yButton | lBumper | rBumper | select | start | lStick | rStick | guide | dPadUp | dPadDown | dPadLeft | dPadRight;
 
 
                     var vjoyInput = new vJoy.JoystickState()
                     {
                         bDevice = (byte)chosenVJoyId,
-                        AxisX = xPad.sThumbLX,
-                        AxisY = -xPad.sThumbLY,
-                        AxisXRot = xPad.sThumbRX,
-                        AxisYRot = xPad.sThumbRY,
+                        AxisX = ((int)xPad.sThumbLX) - initialBias.AxisX,
+                        AxisY = ((int)-xPad.sThumbLY) - initialBias.AxisY,
+                        AxisXRot = xPad.sThumbRX - initialBias.AxisXRot,
+                        AxisYRot = xPad.sThumbRY - initialBias.AxisYRot,
                         AxisZ = xPad.bRightTrigger,
+                        AxisZRot = xPad.bLeftTrigger,
                         Buttons = buttons,
                     };
 
